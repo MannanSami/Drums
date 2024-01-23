fcecf6ec1666dad5d0d444bf57738b39db2e9126
Mannan Sami <mannansami7@gmail.com>
2024-01-23 23:42:52 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function play(e) {
+    const sound = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
+    
+    if (!sound) {
+        return;
+    }
+
+    sound.currentTime = 0;
+    sound.play();
+    key.classList.add('playing');
+
+    setTimeout(() => {
+        key.classList.remove('playing');
+    }, 200);
+}
+
+window.addEventListener('keydown', play);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', revert));
